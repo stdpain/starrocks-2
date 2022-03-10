a6f677b983c4583c87228d13c55afb67fae72a81
stdpain <drfeng08@gmail.com>
2022-03-09 22:31:01 -0800

    [WIP] support more lowcardinality optimization
    1. case when
    2. if
    3. projection predicate
    
    fix check style
    
    tmp save
    
    save code

---
+++ fe/fe-core/src/main/java/com/starrocks/analysis/VSlotRef.java
@@ -0,0 +1,55 @@
+// This file is licensed under the Elastic License 2.0. Copyright 2021-present, StarRocks Limited.
+
+package com.starrocks.analysis;
+
+import com.google.common.base.Preconditions;
+import com.starrocks.catalog.Type;
+import com.starrocks.common.AnalysisException;
+import com.starrocks.thrift.TExprNode;
+import com.starrocks.thrift.TExprNodeType;
+import com.starrocks.thrift.TVSlotRef;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class VSlotRef extends Expr {
+    private static final Logger LOG = LogManager.getLogger(VSlotRef.class);
+
+    private final int slotId;
+    boolean nullable;
+
+    public VSlotRef(int slotId, boolean nullable, Type type) {
+        super();
+        this.slotId = slotId;
+        this.nullable = nullable;
+        this.type = type;
+    }
+
+    @Override
+    protected void analyzeImpl(Analyzer analyzer) throws AnalysisException {
+        LOG.warn("unreachable path");
+        Preconditions.checkState(false);
+    }
+
+    @Override
+    protected String toSqlImpl() {
+        return "<slot " + slotId + ">" + nullable;
+    }
+
+    @Override
+    protected void toThrift(TExprNode msg) {
+        msg.setNode_type(TExprNodeType.VSLOT_REF);
+        msg.setVslot_ref(new TVSlotRef());
+        msg.vslot_ref.setNullable(nullable);
+        msg.vslot_ref.setSlot_id(slotId);
+    }
+
+    @Override
+    public boolean isNullable() {
+        return nullable;
+    }
+
+    @Override
+    public Expr clone() {
+        return new VSlotRef(slotId, nullable, type);
+    }
+}
