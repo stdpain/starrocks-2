7250f057fd059935097bbd26fd738849ac4eb9d0
stdpain <drfeng08@gmail.com>
2023-06-07 16:44:55 +0800

    [BugFix] Fix memtracker may use-after-free
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/udf/java/utils.cpp
+++ be/src/udf/java/utils.cpp
@@ -31,10 +31,14 @@ PromiseStatusPtr call_function_in_pthread(RuntimeState* state, const std::functi
     PromiseStatusPtr ms = std::make_unique<PromiseStatus>();
     if (bthread_self()) {
         state->exec_env()->udf_call_pool()->offer([promise = ms.get(), state, func]() {
-            MemTracker* prev_tracker = tls_thread_status.set_mem_tracker(state->instance_mem_tracker());
-            SCOPED_SET_TRACE_INFO({}, state->query_id(), state->fragment_instance_id());
-            DeferOp op([&] { tls_thread_status.set_mem_tracker(prev_tracker); });
-            promise->set_value(func());
+            Status st;
+            // instance mem tracker maybe free when promise->set_value called.
+            {
+                SCOPED_THREAD_LOCAL_MEM_TRACKER_SETTER(state->instance_mem_tracker());
+                SCOPED_SET_TRACE_INFO({}, state->query_id(), state->fragment_instance_id());
+                st = func();
+            }
+            promise->set_value(st);
         });
     } else {
         ms->set_value(func());
