09c4f186c66fb033bbdd465b8f14032b3cd05079
stdpain <drfeng08@gmail.com>
2023-09-24 15:10:05 +0800

    avoid poller cost too much time
    Signed-off-by: stdpain <drfeng08@gmail.com>

--- be/src/exec/pipeline/pipeline_driver_poller.cpp
+++ be/src/exec/pipeline/pipeline_driver_poller.cpp
@@ -71,6 +71,16 @@ void PipelineDriverPoller::run_internal() {
             auto driver_it = _local_blocked_drivers.begin();
             while (driver_it != _local_blocked_drivers.end()) {
                 auto* driver = *driver_it;
+                auto st = duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count();
+                auto defer = DeferOp([&]() {
+                    auto ed = duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count();
+                    size_t current_hang_time = ed - st;
+                    poller_hang_time = std::max<size_t>(poller_hang_time, current_hang_time);
+                    if (current_hang_time > 50) {
+                        LOG(INFO) << "slow poller detected, cost:" << current_hang_time
+                                  << " driver:" << driver->to_readable_string();
+                    }
+                });
 
                 if (!driver->is_query_never_expired() && driver->query_ctx()->is_query_expired()) {
                     // there are not any drivers belonging to a query context can make progress for an expiration period
